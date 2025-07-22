20860d6a8cdcb458c042c28a53a1ae36f73aa6e8
chlwlgus4 <chlwlgus89@naver.com>
2025-07-22 23:44:45 +0900

    payment 객체 추가중

--- src/main/java/com/courier/payment/PaymentController.java
+++ src/main/java/com/courier/payment/PaymentController.java
@@ -2,18 +2,23 @@ package com.courier.payment;
 
 import com.courier.payment.dto.PaymentConfirmRequest;
 import com.courier.payment.dto.PaymentConfirmResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/api/payment")
 @RestController
 public class PaymentController {
 
+    private final PaymentService paymentService;
+
     @PostMapping("/confirm")
-    public ResponseEntity<PaymentConfirmResponse> confirm(@RequestBody PaymentConfirmRequest dto) {
-        log.info("paymentConfirmDTO: {}", dto);
+    public ResponseEntity<PaymentConfirmResponse> confirm(@RequestBody PaymentConfirmRequest request) {
+        paymentService.confirm(request);
+        log.info("paymentConfirmDTO: {}", request);
         return ResponseEntity.ok(PaymentConfirmResponse.builder()
                 .result(true)
                 .build());
